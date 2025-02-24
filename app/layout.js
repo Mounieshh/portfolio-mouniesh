d4655d0d66d188a2d0397bee3b1263dd5743e857
Mouniesh <iammounieshv@gmail.com>
2025-02-24 20:27:59 +0530

    navbar over

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${spaceGrotesk.variable} antialiased font-sans`}> {/* Use font-sans for Tailwind compatibility */}
+      <body className={`${spaceGrotesk.variable} antialiased font-sans bg-background`}> {/* Use font-sans for Tailwind compatibility */}
         <Navbar />
         <div>
           {children}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,7 +8,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
+        background: "#F1EFED",
         foreground: "var(--foreground)",
       },
       fontFamily :{
